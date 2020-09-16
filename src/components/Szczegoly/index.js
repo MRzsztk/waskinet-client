349737271093158a742932fb4a44c2cd45ded310
MRzsztk <rzeszutek.malgorzata@gmail.com>
2020-09-16 03:18:08 +0200

    add dynamic pricelist

--- src/components/Szczegoly/index.js
+++ src/components/Szczegoly/index.js
@@ -1,24 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import './style.css'
 
-export default function Szczegoly({ usluga }) {
+export default function Szczegoly({ usluga, services }) {
 
     return (
         <div className="szczegoly">
+
+        {/* ----------------internet------------------- */}
                 {usluga==="internet" &&
                 <div>
-                <p>to paragraf o internecie.</p>
-                <p>Należy wpisać tu coś ciekawego.</p>
+                <p>to paragraf o internecie. Należy wpisać tu coś ciekawego, aby przykuć uwagę klienta. Może być też obrazek.</p>
+                <div className="cennik">
+                <div className="cennik-title">CENNIK</div>
+                {services.filter(service => service.category === 'internet').map(service => (
+                <div key={service._id} className="cennik-usluga">
+                    <div className="service-name">- {service.service}:</div>
+                    <div className="service-price"><b>{service.price.toFixed(2)}zł</b></div> 
+                    {service.unit && <div className="service-unit">/{service.unit}</div>}
+                </div>
+        ))}
+                </div>
                 </div>        
                     }
+
+        {/* ----------------telefon------------------- */}
                 {usluga==="telefon" &&
                 <div>
-                    <p>Telefon stacjonarny.</p>
-                    <p>Wynalazek coraz zadziej używany, niestety niezbędny w biznesie. Daje prestiż i powagę firmie. Niezastąpiony w domu np u starszych rodziców.</p>
-                    <p>Oferta dla firm, instytucji i klientów indywidualnych:</p>
-                    <b>Abonament (utrzymanie numeru) 10 zł miesięcznie.</b>
-                    <p>Połączenia w UE (telefony stacjonarne) 10 gr/min.</p>
-                </div>}
+                    <p>Telefon stacjonarny.<br />
+                    Wynalazek coraz zadziej używany, niestety niezbędny w biznesie. Daje prestiż i powagę firmie. Niezastąpiony w domu np u starszych rodziców.<br />
+                    Oferta dla firm, instytucji i klientów indywidualnych:<br />
+                    Abonament (utrzymanie numeru) 10 zł miesięcznie.<br />
+                    Połączenia w UE (telefony stacjonarne) 10 gr/min.</p>
+                    <div className="cennik">
+                <div className="cennik-title">CENNIK</div>
+                {services.filter(service => service.category === 'telefon').map(service => (
+                <div key={service._id} className="cennik-usluga">
+                    <div className="service-name">- {service.service}:</div>
+                    <div className="service-price"><b>{service.price.toFixed(2)}zł</b></div> 
+                    {service.unit && <div className="service-unit">/{service.unit}</div>}
+                </div>
+        ))}
+                </div>
+                </div>
+        }
+
+
+        {/* ----------------hosting------------------- */}
                 {usluga==="hosting" &&
                 <div>
                 <h3>Serwery WWW</h3>
@@ -32,7 +59,39 @@ export default function Szczegoly({ usluga }) {
                     Opłata za HDD 28 zł/100GB</p>
                     <h3>Dedykowane</h3>
                     <p>Oferta personalizowania pod potrzeby indywidualne Klienta. </p>
-                </div>}
+                    
+                    <div className="cennik">
+                <div className="cennik-title">CENNIK</div>
+                <div className="cennik-subcat">SERWERY WWW</div>
+                {services.filter(service => service.category === 'hosting' && service.subcategory === 'www').map(service => (
+                <div key={service._id} className="cennik-usluga">
+                    <div className="service-name">- {service.service}:</div>
+                    <div className="service-price"><b>{service.price.toFixed(2)}zł</b></div> 
+                    {service.unit && <div className="service-unit">/{service.unit}</div>}
+                </div>
+        ))}
+                <div className="cennik-subcat">SERWERY VPS</div>
+                {services.filter(service => service.category === 'hosting' && service.subcategory === 'vps').map(service => (
+                <div key={service._id} className="cennik-usluga">
+                    <div className="service-name">- {service.service}:</div>
+                    <div className="service-price"><b>{service.price.toFixed(2)}zł</b></div> 
+                    {service.unit && <div className="service-unit">/{service.unit}</div>}
+                </div>
+        ))}
+                <div className="cennik-subcat">SERWERY DEDYKOWANE</div>
+                        {services.filter(service => service.category === 'hosting' && service.subcategory === 'dedykowane').map(service => (
+                        <div key={service._id} className="cennik-usluga">
+                            <div className="service-name">- {service.service}</div>
+                            {service.price && <div className="service-price">:<b>{service.price.toFixed(2)}zł</b></div>}
+                            {service.unit && <div className="service-unit">/{service.unit}</div>}
+                        </div>
+                ))}
+                </div>
+                </div>
+            }
+
+
+            {/* ----------------kolokacja------------------- */}
                 {usluga==="kolokacja" &&
                 <div>
                 <p>W naszej nowej serwerowni można wydzierżawić maszynę z dowolnie wybranym systemem oraz o dowolnych parametrach.</p>
--- src/pages/Oferta/index.js
+++ src/pages/Oferta/index.js
@@ -6,20 +6,37 @@ import Header from "../../components/Header";
 import NavbarOferta from "../../components/NavbarOferta";
 import Footer from "../../components/Footer";
 import Szczegoly from "../../components/Szczegoly";
-// import Internet from "../../components/Internet";
-// import Telefon from "../../components/Telefon";
-// import Kolokacja from "../../components/Kolokacja";
-// import Hosting from "../../components/Hosting";
+
+import api from "../../services/api";
+
 import "./style.css";
 
 export default function Oferta() {
-const [usluga, setUsluga] = useState('internet');
+
 const [error, setError] = useState(null);
 const history = useHistory();
+const [usluga, setUsluga] = useState('hosting');
+const [services, setServices] = useState([])
+
+const getServices = async () => {
+//   setServices([{_id: '00000000', service: "pobieram ofertę..."}])
+  try {
+      const { data } = await api.get("/uslugi");
+      if (data?.hasOwnProperty('error')) {
+          return setError(data.error)
+      }
+      setServices(data)
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+useEffect(() => {
+  getServices()
+}, [])
 
 useEffect(() => {
     setUsluga(usluga);
-    console.log(usluga);
 }, [usluga])
 
     return(
@@ -27,7 +44,8 @@ useEffect(() => {
             <Header />
             <div className="mid">
             <div className="mid-container">
-            {<Szczegoly usluga={usluga} />}
+            {console.log(services)}
+            {<Szczegoly usluga={usluga} services={services} />}
             <NavbarOferta handlerOnClick={(e) => setUsluga(e.target.name)} />
             </div>
             </div>
